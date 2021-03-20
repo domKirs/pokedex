80149471e1199c7746fdbce70c8dc06e85b79824
domKirs <41889335+domKirs@users.noreply.github.com>
2021-03-20 02:02:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ fetch('https://pokeapi.co/api/v2/pokemon/1')
     initialize(json);
   });
 
-
 const initialize = function({height, id, name, sprites, types, weight}) {
 
   let imgUrl = sprites.versions['generation-i']['red-blue'].front_gray;
@@ -20,14 +19,22 @@ const initialize = function({height, id, name, sprites, types, weight}) {
     }
   }); 
 
+  id = '' + id;
+  if (id.length === 1) {
+    id = '00' + id;
+  } else if (id.length === 2) {
+    id = '0' + id;
+  }
+
   const main = document.querySelector('main');
   const imgNode = document.createElement('img');
+  imgNode.setAttribute('src', imgUrl);
+  main.appendChild(imgNode);
+  
   console.log(height);
   console.log(id);
   console.log(name);
   console.log(imgUrl);
   console.log(type);
   console.log(weight)
-  imgNode.setAttribute('src', imgUrl);
-  main.appendChild(imgNode);
 }
