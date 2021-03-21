7ea63c8d5237a85f9834e980c83ee52f3d155f36
domKirs <41889335+domKirs@users.noreply.github.com>
2021-03-21 15:36:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,68 @@
-fetch('https://pokeapi.co/api/v2/pokemon/1')
+const pokemonDataBasic = new Map();
+const pokemonDataDetail = new Map();
+const initialUrl = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151';
+const main = document.querySelector('main');
+const screen = document.querySelector('.screen');
+
+fetch(initialUrl) 
+  .then(response => checkIfOk(response))
   .then(response => response.json())
-  .then(json => {
-    console.log(json);
-    initialize(json);
-  });
+  .then(data => saveDataBasic(data))
+  .then(map => updateMain(map))
+  .catch(error => console.log(error));
 
-const initialize = function({height, id, name, sprites, types, weight}) {
-
-  let imgUrl = sprites.versions['generation-i']['red-blue'].front_gray;
-
-  let type = '';
-  types.forEach((entry, index) => {
-    if (index === 0) {
-      type += entry.type.name;
-    } else {
-      type += ' ';
-      type += `/ ${entry.type.name}`;
-    }
-  }); 
-
-  id = '' + id;
-  if (id.length === 1) {
-    id = '00' + id;
-  } else if (id.length === 2) {
-    id = '0' + id;
+function checkIfOk(response) {
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
   }
-
-  const main = document.querySelector('main');
-  const imgNode = document.createElement('img');
-  imgNode.setAttribute('src', imgUrl);
-  main.appendChild(imgNode);
-  
-  console.log(height);
-  console.log(id);
-  console.log(name);
-  console.log(imgUrl);
-  console.log(type);
-  console.log(weight)
+  return response;
 }
+
+function idCalculation(index) {
+  index += 1;
+  index += '';
+  switch(index.length) {
+    case 1: index = '00' + index; break;
+    case 2: index = '0' + index; break;
+    default: index = index;
+  }
+  return index;
+}
+
+function saveDataBasic({results}) {
+  results.forEach((element, index) => {
+    const id = idCalculation(index);
+    pokemonDataBasic.set(element.name, {id: id, url: element.url});
+  });
+  return Promise.resolve(pokemonDataBasic);
+}
+
+function updateMain(map){
+  for(const [key, value] of map) {
+    const div = document.createElement('div');
+    div.setAttribute('name', key);
+    div.innerText = `${value.id} ${key}`;
+    div.addEventListener('click', onClick);
+    main.appendChild(div);
+  }
+}
+
+function updateSceen({sprites}) {
+  const spriteUrl = sprites.versions['generation-i']['red-blue'].front_gray
+  if(screen.firstChild) {
+    screen.removeChild(screen.firstChild);
+  }
+  let img = document.createElement('img');
+  img.setAttribute('src', spriteUrl);
+  screen.appendChild(img);
+}
+
+function onClick(event) {
+  const name = event.target.getAttribute('name');
+  fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then(response =>  checkIfOk(response))
+    .then(response => response.json())
+    .then(data => updateSceen(data));
+}
+
+
